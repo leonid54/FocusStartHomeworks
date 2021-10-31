04cbca094d53dfa4f24bad30e3fadff787deaec7
Leonid <leongorbuncov3@gmail.com>
2021-10-31 22:45:19 +0700

    (homework-1) поправил логику и баги

--- Homework1/Homework1/main.swift
+++ Homework1/Homework1/main.swift
@@ -1,6 +1,6 @@
 import Foundation
 
-struct Car: Equatable{
+struct Car {
     var manufacturer: String
     var model: String
     var body: Body
@@ -8,89 +8,106 @@ struct Car: Equatable{
     var carNumber: String?
 }
 
-enum Body: Int, CaseIterable {
+enum Body {
     case sedan
     case coupe
     case cabriolet
+    case nonSelectedBody
 }
 
-var array = [Car]()
+var ListOfCars = [Car]()
 
 func printMenu() {
     print("""
-What are you want to do?
-1 - Do you want append a new car?
-2 - Do you want see all cars?
-3 - Do you want see kind cars for body?
-""")
+    What are you want to do?
+    1 - Do you want append a new car?
+    2 - Do you want see all cars?
+    3 - Do you want see kind cars for body?
+    0 - Do you want go away?
+    """)
+    
+    let answer: Bool = true
+    
+    while answer {
+        let choice = readLine()
+        if choice == "1" {
+            assembleNewCar()
+        } else if choice == "2" {
+            showAllCars()
+        } else if choice == "3" {
+            showFilteredCars()
+        } else if choice == "0" {
+            print("See you later, good bye!")
+            break
+        }
+    }
+}
+
+func assembleNewCar() {
+    let manufacturer: String
+    let model: String
+    let body: Body
+    let yearOfIssue: Int?
+    let carNumber: String?
+    
+    print("Write manufacturer of the car")
+    manufacturer = readLine() ?? ""
+    
+    print("Write model of the car")
+    model = readLine() ?? ""
+    
+    print("""
+    Write body of the car:
+    1 - sedan
+    2 - coupe
+    3 - cabriolet
+    """)
+    let typeOfBody = readLine()
+    body = selectBody(typeOfBody: typeOfBody ?? "")
+    
+    print("Write year of the car issue")
+    yearOfIssue = Int(readLine() ?? "-")
+    
+    print("Write the car number")
+    carNumber = readLine() ?? "-"
+    appendAssembledCar(newCar: Car(manufacturer: manufacturer, model: model, body: body,
+                                   yearOfIssue: yearOfIssue, carNumber: carNumber))
+}
+
+func appendAssembledCar(newCar : Car) {
+    ListOfCars.append(newCar)
+}
+
+func selectBody(typeOfBody: String) -> Body{
+    switch (typeOfBody) {
+    case "1":
+        return .sedan
+    case "2":
+        return .coupe
+    case "3":
+        return .cabriolet
+    default:
+        return .nonSelectedBody
+    }
+}
+
+func showAllCars() {
+    print(ListOfCars)
+}
+
+func showFilteredCars() {
+    print("""
+    Choose cars for body:
+    1 - sedan
+    2 - coupe
+    3 - cabriolet
+    """)
+    let typeOfBody = readLine()
+    let selectedBody = selectBody(typeOfBody: typeOfBody ?? "")
+    let filteredList = ListOfCars.filter {
+        $0.body == selectedBody
+    }
+    print(filteredList)
 }
 
 printMenu()
-while let answer = readLine() {
-    if answer == "1" {
-        print("Write manufacturer of the car")
-        let manuf = readLine()
-        print("Write model of the car")
-        let mod = readLine()
-        print("Write year of the car issue")
-        let issue = readLine()
-        print("Write the car number")
-        let number = readLine()
-        print("""
-Write body of the car:
-0 - sedan
-1 - coupe
-2 - cabriolet
-""")
-            if let bod = readLine() {
-                var boddy = Body(rawValue: Int(bod) ?? 0)
-                switch boddy  {
-                case .cabriolet:
-                    print("you choose carbriolet")
-                    array.append(Car(manufacturer: manuf ?? "", model: mod ?? "", body:.cabriolet  , yearOfIssue: Int(issue ?? "-"), carNumber: number ?? ""))
-                case .coupe:
-                    print("you choose coupe")
-                    array.append(Car(manufacturer: manuf ?? "", model: mod ?? "", body:.coupe  , yearOfIssue: Int(issue ?? "-"), carNumber: number ?? ""))
-                case .sedan:
-                    print("you choose sedan")
-                    array.append(Car(manufacturer: manuf ?? "", model: mod ?? "", body: .sedan  , yearOfIssue: Int(issue ?? "-"), carNumber: number ?? ""))
-                default:
-                    print("you choose nothing")
-                }
-        }
-        printMenu()
-    } else if answer == "2" {
-    print(array)
-    printMenu()
-    } else if answer == "3" {
-        print("""
-Choose cars for body:
-1 - cabriolet
-2 - coupe
-3 - sedan
-""")
-        while let choose = readLine() {
-            if choose == "1" {
-                let filter = array.filter { car in
-                    return car.body == .cabriolet
-                }
-                print(filter)
-                break
-            }
-            else if answer == "2" {
-                let filter = array.filter { car in
-                    return car.body == .coupe
-                }
-                print(filter)
-                break
-            }  else if answer == "3" {
-                let filter = array.filter { car in
-                    return car.body == .sedan
-                }
-                print(filter)
-                break
-            }
-        }
-        printMenu()
-    }
-}
